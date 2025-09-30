8938b18093d189cff349f974bbbaff728249349d
jorgecahui <jorgecahui347@gmail.com>
2025-09-30 09:37:19 -0500

    kl

--- ms-producto/src/main/java/com/example/ms_producto/feign/CatalogoFeign.java
+++ ms-producto/src/main/java/com/example/ms_producto/feign/CatalogoFeign.java
@@ -1,6 +1,7 @@
 package com.example.ms_producto.feign;
 
 import com.example.ms_producto.dto.CategoriaDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,5 +9,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ms-catalogo", path = "/categoria")
 public interface CatalogoFeign {
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "categoriaListarPorIdCB", fallbackMethod = "fallbackCategoria")
     CategoriaDto buscarPorId(@PathVariable("id") Integer id);
+
+    default CategoriaDto fallbackProducto(Integer id, Exception e) {
+        CategoriaDto CategoriaDto = new CategoriaDto();
+        CategoriaDto.setId(9000000);
+        return CategoriaDto;
+    }
+
 }
